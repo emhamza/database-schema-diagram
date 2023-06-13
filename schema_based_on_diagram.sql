7900a77ce9fb7754e5660843e20a458d328b0c0e
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-06-13 18:45:42 +0300

    Add updated schema_based_on_diagram.sql with changes implemented.

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,44 @@
+-- Table: Patient
+CREATE TABLE Patient (
+  id INT PRIMARY KEY,
+  name VARCHAR(100),
+  date_of_birth DATE
+);
+
+-- Table: Medical_Histories
+CREATE TABLE Medical_Histories (
+  id INT PRIMARY KEY,
+  admitted_at TIMESTAMP,
+  patient_id INT,
+  status VARCHAR(100),
+  FOREIGN KEY (patient_id) REFERENCES Patient(id)
+);
+
+-- Table: Invoices
+CREATE TABLE Invoices (
+  id INT PRIMARY KEY,
+  total_amount DECIMAL(10, 2),
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT,
+  FOREIGN KEY (medical_history_id) REFERENCES Medical_Histories(id)
+);
+
+-- Table: Treatments
+CREATE TABLE Treatments (
+  id INT PRIMARY KEY,
+  type VARCHAR(100),
+  name VARCHAR(100)
+);
+
+-- Table: Invoice_Items
+CREATE TABLE Invoice_Items (
+  id INT PRIMARY KEY,
+  unit_price DECIMAL(10, 2),
+  quantity INT,
+  total_price DECIMAL(10, 2),
+  invoice_id INT,
+  treatment_id INT,
+  FOREIGN KEY (invoice_id) REFERENCES Invoices(id),
+  FOREIGN KEY (treatment_id) REFERENCES Treatments(id)
+);
